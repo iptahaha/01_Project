316a0783f7060b3d7595386131e3ff13ff709a11
Nataliia Sychevska <petriwna43@gmail.com>
2021-12-27 17:15:31 +0200

    fix gulp and folder web

---
+++ web/src/index.js
@@ -0,0 +1,15 @@
+let tabContent = document.getElementsByClassName('tab-content')
+let tabLink = document.getElementsByClassName('tab-link');
+
+function openTab(ev, tabName) {
+
+    for (let i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    for (let i = 0; i < tabLink.length; i++){
+        tabLink[i].className = tabLink[i].className.replace(" active", "")
+    }
+    document.getElementById(tabName).style.display = "block";
+    ev.currentTarget.className += " active";
+}
